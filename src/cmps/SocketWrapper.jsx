9972eee0fe7475b809e17f976e9881341d74b357
yaz101092 <yazmeray123@gmail.com>
2025-06-04 12:32:39 +0300

    edit socketwrapper

--- src/cmps/SocketWrapper.jsx
+++ src/cmps/SocketWrapper.jsx
@@ -10,34 +10,31 @@ export function SocketWrapper({ children }) {
   const dispatch = useDispatch()
   const user = useSelector(state => state.userModule.user)
 
-  // 1. אתחול חיבור Socket.IO פעם אחת כשהאפליקציה עולה
   useEffect(() => {
     socketService.setup()
   }, [])
 
-  // 2. בכל פעם שהמשתמש משתנה
+
   useEffect(() => {
     if (user && user._id) {
-      // א. שליחת userId לשרת
       socketService.setUser(user._id)
 
-      // ב. הטיפול באירוע "order-added"
+
       socketService.onNewOrder(order => {
         showSuccessMsg('New order received!')
         dispatch({ type: ADD_ORDER, order })
       })
 
-      // ג. הטיפול באירוע "order-status-updated"
       socketService.onOrderStatusUpdated(order => {
         showSuccessMsg('Order status updated!')
         dispatch({ type: UPDATE_ORDER, order })
       })
     } else {
-      // אם אין user, מנתקים ומנקים listeners
+
       socketService.unsetUser()
     }
   }, [user, dispatch])
 
-  // עטיפת children בקלאס ייעודי
+
   return <div className="socket-wrapper">{children}</div>
 }
